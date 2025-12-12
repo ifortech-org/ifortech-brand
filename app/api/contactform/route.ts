2b1c377e36a6d0409cc95c0a11ee4bc77af7c467
erasmosalsanoifortech <e.salsano@ifortech.com>
2025-12-12 17:18:15 +0100

    Riproviamo ancora

--- app/api/contactform/route.ts
+++ app/api/contactform/route.ts
@@ -49,38 +49,38 @@ export async function POST(request: Request) {
     // });
 
     // CONFIGURAZIONE DA NEWS.INTEGYS
-    const transporter = nodemailer.createTransport({
-      host: "smtp-mail.outlook.com",
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      tls: {
-        minVersion: 'TLSv1.2',
-        rejectUnauthorized: true,
-      },
-      auth: {
-        user: mailSenderAccount.user,
-        pass: mailSenderAccount.pass,
-      },
-      // Timeout più lungo per Office 365
-      connectionTimeout: 60000,
-      greetingTimeout: 30000,
-    });
-
-    // CONFIGURAZIONE DA CLEWAY
     // const transporter = nodemailer.createTransport({
-    //   host: "smtp.office365.com",
+    //   host: "smtp-mail.outlook.com",
     //   port: 587,
     //   secure: false,
+    //   requireTLS: true,
     //   tls: {
-    //     ciphers: "SSLv3",
-    //     rejectUnauthorized: false,
+    //     minVersion: 'TLSv1.2',
+    //     rejectUnauthorized: true,
     //   },
     //   auth: {
     //     user: mailSenderAccount.user,
     //     pass: mailSenderAccount.pass,
     //   },
+    //   // Timeout più lungo per Office 365
+    //   connectionTimeout: 60000,
+    //   greetingTimeout: 30000,
     // });
+
+    // CONFIGURAZIONE DA CLEWAY
+    const transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false,
+      tls: {
+        ciphers: "SSLv3",
+        rejectUnauthorized: false,
+      },
+      auth: {
+        user: mailSenderAccount.user,
+        pass: mailSenderAccount.pass,
+      },
+    });
 
     const mailData = {
       from: mailSenderAccount.email,
